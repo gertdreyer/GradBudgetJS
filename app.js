cb166e25d0a2b2f3d17dd97b38e0baf80d47f83a
Gert Dreyer <gert@bbd.co.za>
2021-05-07 18:24:10 +0200

    Missing quotes on default color

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
     console.log(req.query)
-  let contents = {name: "World", params: `<h2 style='color:${req.query.color ?? black}'>${req.query.text ?? ""}</h2>`}
+  let contents = {name: "World", params: `<h2 style='color:${req.query.color ?? "black"}'>${req.query.text ?? ""}</h2>`}
   res.render('home',contents)
 })
 
