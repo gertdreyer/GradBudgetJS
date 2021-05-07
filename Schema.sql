21ad9e7be0a52551300b1ee740a6f0c2167b7c55
Gert Dreyer <gert@bbd.co.za>
2021-05-07 21:15:42 +0200

    Provisional Schema

---
+++ Schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS Users (
+UserID bigserial PRIMARY KEY NOT NULL,
+Oauthlink varchar(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS UserCatagories(
+UserCategoryID bigserial PRIMARY KEY NOT NULL,
+CatagoryName varchar(30) NOT NULL,
+UserID BIGINT NOT NULL,
+Budget money NULL, 
+FOREIGN KEY UserID REFERENCES Users(UserID);
+);
+
+CREATE TABLE UserTransactions(
+UserExpenceID BIGSERIAL PRIMARY KEY NOT NULL,
+Description VARCHAR(30) NOT NULL,
+Amount Money NOT NULL,
+CategoryID BIGINT,
+FOREIGN KEY UserID REFERENCES Users(UserID)
+FOREIGN KEY CategoryID REFERENCES UserCatagories(UserCategoryID)
+);
