44fb805e010f4fb4d07c987b94c4c1d06ce3d22b
Gert Dreyer <gert@bbd.co.za>
2021-05-08 10:12:56 +0200

    Added Auth

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 
 const express = require('express')
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config()
+
+
 
 const app = express()
 const port = process.env.PORT ?? 3000
 
+app.use(auth(JSON.parse(process.env.AUTH_CONF)))
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.json(req.oidc.user);
+});
 app.get('/', (req, res) => {
     console.log(req.query)
-  let contents = {name: "World", params: `<h2 style='color:${req.query.color ?? "black"}'>${req.query.text ?? ""}</h2>`}
-  res.render('home',contents)
+    let contents = { name: "World", params: `<h2 style='color:${req.query.color ?? "black"}'>${req.query.text ?? ""}</h2>` }
+    res.render('home', contents)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
