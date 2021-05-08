49d7af3c09da1cc436ccf3775148d13a05ba9a45
Gert Dreyer <gert@bbd.co.za>
2021-05-08 10:58:36 +0200

    Added Linting

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
+const express = require("express");
+var exphbs = require("express-handlebars");
+const { auth, requiresAuth } = require("express-openid-connect");
+require("dotenv").config();
 
-const express = require('express')
-var exphbs = require('express-handlebars');
-const { auth, requiresAuth } = require('express-openid-connect');
-require('dotenv').config()
+const app = express();
+const port = process.env.PORT ?? 3000;
 
+app.use(auth(JSON.parse(process.env.AUTH_CONF)));
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-
-const app = express()
-const port = process.env.PORT ?? 3000
-
-app.use(auth(JSON.parse(process.env.AUTH_CONF)))
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-
-app.get('/profile', requiresAuth(), (req, res) => {
+app.get("/profile", requiresAuth(), (req, res) => {
     res.json(req.oidc.user);
 });
-app.get('/', (req, res) => {
-    console.log(req.query)
-    let contents = { name: "World", params: `<h2 style='color:${req.query.color ?? "black"}'>${req.query.text ?? ""}</h2>` }
-    res.render('home', contents)
-})
+app.get("/", (req, res) => {
+    console.log(req.query);
+    let contents = {
+        name: "World",
+        params: `<h2 style='color:${req.query.color ?? "black"}'>${
+            req.query.text ?? ""
+        }</h2>`,
+    };
+    res.render("home", contents);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
+    console.log(`Example app listening at http://localhost:${port}`);
+});
