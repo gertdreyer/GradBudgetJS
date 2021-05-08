d8322fbb1a9b06858f408d5853517d397ffa18b6
Luke van Aardt <vanaardtluke@gmail.com>
2021-05-08 10:35:44 +0200

    separated routes into router file

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-
 const express = require('express')
-var exphbs = require('express-handlebars');
-const { auth, requiresAuth } = require('express-openid-connect');
-require('dotenv').config()
+const exphbs = require('express-handlebars');
+const router = require('./router');
+const { auth } = require('express-openid-connect');
 
+require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT ?? 3000;
 
-const app = express()
-const port = process.env.PORT ?? 3000
-
-app.use(auth(JSON.parse(process.env.AUTH_CONF)))
+app.use(auth(JSON.parse(process.env.AUTH_CONF)));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-
-app.get('/profile', requiresAuth(), (req, res) => {
-    res.json(req.oidc.user);
-});
-app.get('/', (req, res) => {
-    console.log(req.query)
-    let contents = { name: "World", params: `<h2 style='color:${req.query.color ?? "black"}'>${req.query.text ?? ""}</h2>` }
-    res.render('home', contents)
-})
+app.use('/',router);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
+    console.log(`Example app listening at http://localhost:${port}`);
+});
