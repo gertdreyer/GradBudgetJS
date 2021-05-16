f64d8ff4aaac70a496d9fd7f6646accac42c529a
Gert Dreyer <gert@bbd.co.za>
2021-05-16 10:42:06 +0200

    Fixed DB connection

--- queries/db.js
+++ queries/db.js
@@ -1,10 +1,9 @@
 const { Client } = require("pg");
+require("dotenv").config();
+
 const client = new Client({
-    host: process.env.DATABASE_SERVER || "localhost",
-    port: process.env.DATABASE_PORT || 5432,
-    user: process.env.DATABASE_USER || "postgres",
-    password: process.env.DATABASE_PASSWORD || "postgres",
-    database: process.env.DATABASE_NAME || "postgres",
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
 });
 
 client
