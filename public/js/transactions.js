c1e1d514ba105cc25dbb73a2ee9f41ad670f5957
Luke van Aardt <vanaardtluke@gmail.com>
2021-05-20 07:49:08 +0200

    editing transactions implemented

---
+++ public/js/transactions.js
@@ -0,0 +1,30 @@
+const submitBtn = document.getElementById("put-transaction");
+
+const sendHTTP = async (url, methodType, data) => {
+    if (url && data) {
+        console.log(data);
+        await fetch(url, {
+            method: methodType,
+            body: JSON.stringify(data),
+            headers: { "Content-Type": "application/json" },
+        });
+    }
+};
+
+const putTransactions = async () => {
+    const id = document.getElementById("id").value;
+    const desc = document.getElementById("desc");
+    const am = document.getElementById("amount");
+
+    const data = {
+        userid: id,
+        description: desc.value,
+        amount: am.value,
+    };
+
+    await sendHTTP(`/transactions/${id}`, "PUT", data);
+
+    window.location = "/transactions";
+};
+
+submitBtn.addEventListener("click", () => putTransactions());
