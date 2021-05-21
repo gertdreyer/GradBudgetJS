f7f875e30e1648fb934a4437e28f50d5e7cabfe1
Michael Teles <michael.teles@vcontractor.co.za>
2021-05-21 10:10:17 +0200

    Add logout button.

--- routes/navlist.js
+++ routes/navlist.js
@@ -1,6 +1,7 @@
 const navlist = [
     { name: "Budget", href: "/budgets" },
     { name: "Transactions", href: "/transactions" },
+    { name: "Logout", href: "/logout" },
 ];
 
 module.exports = navlist;
